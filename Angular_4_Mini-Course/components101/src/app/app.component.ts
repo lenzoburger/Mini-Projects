f20f1d893dd148bf435d3918a43f287b96865675
Lencho Burka <lencho.burka@asb.co.nz>
2019-02-13 19:38:55 +1300

    Added Angular 4 Mini Coure + Add gitignore per sub-project

---
+++ Angular_4_Mini-Course/components101/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>Hey Guys!</h1>
+<ul><b>Customer Details</b>
+    <li>{{customerObj.name}}</li>
+    <li>{{customerObj.age}}</li>
+    <li>{{customerObj.country}}</li>
+</ul>
+
+
+<ul><b>Shopping List</b>
+    <li *ngFor="let items of myShoppingList">{{items}}</li>
+</ul>
+
+  <h3 *ngIf="beNice; else otherTmpl">You're Awesome</h3>
+  <ng-template #otherTmpl>You're Ok</ng-template>
+
+<p></p>
+  <div *ngIf="isRaining; then rainTmpl else sunTmpl;"></div>
+    <ng-template #rainTmpl>It's raining</ng-template>
+    <ng-template #sunTmpl>It's Sunny</ng-template>
+`
+  ,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  customerObj = {
+    name: 'lencho',
+    age: 190,
+    country: 'MarsOpia'
+  }
+  myShoppingList = ['fish','banana','mylk','veges']
+  beNice = false;
+  isRaining = false;
+}
