8f2ef815411ec46edfed5b2e03b443cff54d106d
lenzoburger <lenzoburger@gmail.com>
2019-09-13 18:06:28 +1000

    upgrade lazy-laoding-app to angular 8

--- Angular/lazy-loading-app/e2e/src/app.e2e-spec.ts
+++ Angular/lazy-loading-app/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to lazy-loading-app!');
+    expect(page.getTitleText()).toEqual('lazy-loading-app app is running!');
   });
 
   afterEach(async () => {
